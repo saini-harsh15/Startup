eb0c2fd6e02d6a7e77c8ce61939c38a294eb8673
Harsh Saini <harshsaini2462@gmail.com>
2026-04-26 16:49:36 +0530

    Deployment

--- src/main/java/org/example/startupecosystem/StartupEcosystemApplication.java
+++ src/main/java/org/example/startupecosystem/StartupEcosystemApplication.java
@@ -2,14 +2,18 @@ package org.example.startupecosystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StartupEcosystemApplication {
+public class StartupEcosystemApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StartupEcosystemApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StartupEcosystemApplication.class, args);
     }
-
 }
-
-
